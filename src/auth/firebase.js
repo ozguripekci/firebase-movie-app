d22c09218c12e01e337ed88ecf2c5560b41a1548
ozgur.ipekci <70923024+ozguripekci@users.noreply.github.com>
2022-04-13 23:13:21 +0200

    app created

---
+++ src/auth/firebase.js
@@ -0,0 +1,102 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+const firebaseConfig = {
+  apiKey: "AIzaSyAgvexaYMJuQ4XtOsD-JHkOH_Q-Q-xt1uQ",
+  authDomain: "movie-app-10074.firebaseapp.com",
+  projectId: "movie-app-10074",
+  storageBucket: "movie-app-10074.appspot.com",
+  messagingSenderId: "954831977845",
+  appId: "1:954831977845:web:2ab51631a12d1fc2a3a73c",
+  measurementId: "G-0T9RBQJW3V"
+};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCg1fCEO8i7MQnM3SiMJPiVcADp_NI9XTw",
+//   authDomain: "movie-app-1-6ec44.firebaseapp.com",
+//   projectId: "movie-app-1-6ec44",
+//   storageBucket: "movie-app-1-6ec44.appspot.com",
+//   messagingSenderId: "950886341278",
+//   appId: "1:950886341278:web:f4feb188157227da55398f",
+// };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async (email, password, displayName, navigate) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const logOut = () => {
+  signOut(auth);
+  alert("logged out successfully");
+};
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+      // ...
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
+};
---
+++ src/router/AppRouter.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
